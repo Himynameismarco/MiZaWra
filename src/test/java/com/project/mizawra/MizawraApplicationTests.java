f96138f8b2d413bc001b6a2e615036a152206bc0
Anton_Mishchuk <amishchuk@vertafore.com>
2023-01-17 23:02:00 +0200

    init commit

---
+++ src/test/java/com/project/mizawra/MizawraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mizawra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MizawraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
