d9506ed053fe847a2b75f7e06aa08e755bde9373
Anton_Mishchuk <amishchuk@vertafore.com>
2023-01-17 22:45:51 +0200

    init commit
    (cherry picked from commit 84da40f788b26f2eedb0e3224a362043013c658e)

---
+++ mizawi/src/main/java/com/mizawi/MizawiApplication.java
@@ -0,0 +1,13 @@
+package com.mizawi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MizawiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MizawiApplication.class, args);
+	}
+
+}
