65bda10ef0726452a5f8c435c8df1f419debd1dc
Anton_Mishchuk <amishchuk@vertafore.com>
2023-01-29 20:45:29 +0200

    add user detailed service to our system

--- src/main/java/com/project/mizawra/config/SecurityConfiguration.java
+++ src/main/java/com/project/mizawra/config/SecurityConfiguration.java
@@ -1,27 +1,33 @@
 package com.project.mizawra.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-  @Bean
-  public UserDetailsService userDetailsService() {
-    return new InMemoryUserDetailsManager(User.withUsername("user1")
-            .password(passwordEncoder().encode("user1Pass"))
-            .roles("USER")
-            .build());
+  @Autowired
+  private UserDetailsService userDetailsService;
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) {
+    auth.authenticationProvider(authProvider());
+  }
+
+  public DaoAuthenticationProvider authProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
   }
 
   @Bean
@@ -30,7 +36,7 @@ public class SecurityConfiguration {
         .requestMatchers("/login*").permitAll()
         .anyRequest().authenticated()
         .and()
-        .formLogin().loginPage("/login").loginProcessingUrl("/perform_login") // TODO: Create this page - the URL to submit the username and password to
+        .formLogin().loginPage("/login")
         .defaultSuccessUrl("/home", true)
         .failureUrl("/login_error") //TODO: Create this page
         .and()
@@ -38,7 +44,6 @@ public class SecurityConfiguration {
     return http.build();
   }
 
-  @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
---
+++ src/main/java/com/project/mizawra/service/SystemUserDetailedService.java
@@ -0,0 +1,32 @@
+package com.project.mizawra.service;
+
+import com.project.mizawra.dao.ClientRepository;
+import com.project.mizawra.models.Client;
+import java.util.ArrayList;
+import java.util.Optional;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailsService")
+public class SystemUserDetailedService implements UserDetailsService {
+    private final ClientRepository clientRepository;
+
+    public SystemUserDetailedService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Client> optionalClient = clientRepository.findByEmail(username);
+        if (optionalClient.isPresent()) {
+            Client client = optionalClient.get();
+
+            //TODO think do we need authorities or not (3 parameter in constructor)
+            return new User(client.getEmail(), client.getPassword(), new ArrayList<>());
+        }
+        throw new UsernameNotFoundException("User is not found");
+    }
+}
