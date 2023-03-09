e63cc0761a27ef14ef464216922898247bd6bf7c
Anton_Mishchuk <amishchuk@vertafore.com>
2023-03-09 23:38:40 +0200

    add prompt logic

--- src/main/java/com/project/mizawra/dao/PromptRepository.java
+++ src/main/java/com/project/mizawra/dao/PromptRepository.java
@@ -1,8 +1,11 @@
 package com.project.mizawra.dao;
 
+import com.project.mizawra.models.Mode;
 import com.project.mizawra.models.Prompt;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PromptRepository extends JpaRepository<Prompt, UUID> {
+    List<Prompt> findByMode(Mode mode);
 }
