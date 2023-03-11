e1bb31966f4b1cfe63570f0a62a931d7a3792561
Anton_Mishchuk <amishchuk@vertafore.com>
2023-03-11 15:13:02 +0200

    add timer logic

--- src/main/resources/static/js/writing.js
+++ src/main/resources/static/js/writing.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", init);
 
+let isPaused = false;
+
 function init() {
+    if(document.querySelector('.time-container h1')) {
+        const duration = 60*15;
+        const timeContainer = document.querySelector('.time-container');
+
+        timeContainer.addEventListener('click', ()=> {
+            isPaused = !isPaused;
+        });
+
+        startTimer(duration, timeContainer.querySelector('h1'));
+    }
+
     document.getElementById('save').addEventListener('click', ()=>{
         const mode = document.getElementById('prompt')?.getAttribute('mode');
         const title = document.getElementById('title').value;
@@ -12,4 +25,23 @@ function init() {
             window.location.href = "/home";
         });
     });
+}
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        if (!isPaused) {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                timer = duration;
+            }
+        }
+    }, 1000);
 }
