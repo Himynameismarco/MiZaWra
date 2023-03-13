38edc71352dd825e3eac0b210eafcd4d0699dd47
Anton_Mishchuk <amishchuk@vertafore.com>
2023-03-13 22:53:55 +0200

    add infinite scroll

--- src/main/java/com/project/mizawra/dao/JournalRepository.java
+++ src/main/java/com/project/mizawra/dao/JournalRepository.java
@@ -2,9 +2,13 @@ package com.project.mizawra.dao;
 
 import com.project.mizawra.models.Client;
 import com.project.mizawra.models.Journal;
+import java.util.List;
 import java.util.UUID;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface JournalRepository extends JpaRepository<Journal, UUID> {
+    List<Journal> findAllByOwner(Client owner, Pageable pageable);
     long countByOwner(Client owner);
 }
