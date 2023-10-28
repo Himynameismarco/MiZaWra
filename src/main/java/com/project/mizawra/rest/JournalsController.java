83cd4a54975facccc45e01751b2df90a635e1ffa
Anton_Mishchuk <amishchuk@vertafore.com>
2023-10-28 13:07:37 +0300

    fix body for save request

--- src/main/java/com/project/mizawra/rest/JournalsController.java
+++ src/main/java/com/project/mizawra/rest/JournalsController.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class JournalsController {
     }
 
     @PostMapping("/save")
-    public JournalDto saveJournal(JournalDto journalDto) throws Exception {
+    public JournalDto saveJournal(@RequestBody JournalDto journalDto) throws Exception {
         return convertJournalToDto(journalService.save(journalDto));
     }
 
